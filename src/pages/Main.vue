79a98f468932d466c0f7a5031113ad06397f9219
hovo333 <hovhannes.khazaryan@gmail.com>
2022-09-19 23:37:35 +0400

    1. Was created vue project. 2. Was integrated tailwindcss package. 3. Was created dropdown component. 4. Was added filter actions for dropdown component.

---
+++ src/pages/Main.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hg-main-page">
+    <organism-dropdown v-if="dropdownData" :dropdown-data="dropdownData"/>
+  </div>
+</template>
+
+<script>
+import OrganismDropdown from '@/components/Dropdown';
+
+export default {
+  name: "Main-page",
+  components: {
+    OrganismDropdown
+  },
+  data() {
+    return {
+      dropdownTitle: 'Main',
+      dropdownData: [
+        {
+          id: 1014,
+          order: 2,
+          hide: false
+        },
+        {
+          id: 4352,
+          order: 3,
+          hide: true
+        },
+        {
+          id: 23,
+          order: 1,
+          hide: false
+        },
+        {
+          id: 144,
+          order: 4,
+          hide: false
+        },
+        {
+          id: 543,
+          order: 6,
+          hide: false
+        },
+        {
+          id: 5719,
+          order: 5,
+          hide: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/styles/pages/Main.scss'
+</style>
